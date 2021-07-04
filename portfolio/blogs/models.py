6f140e7a62e345028d79b91ad38e6d17b097db5b
Ishan Sharma <ishan@mtlbigger.com>
2021-07-04 13:52:45 +0530

    almost done

--- portfolio/blogs/models.py
+++ portfolio/blogs/models.py
@@ -6,3 +6,9 @@ class Blog(models.Model):
     pub_date = models.DateField()
     body = models.TextField()
     image = models.ImageField(upload_to = 'images/')
+
+    def summary(self):
+        return self.body[:10]+' ...'
+
+    def __str__(self):
+        return self.title
--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views as jview
 urlpatterns = [
     path('',jview.home,name='home'),
     path('admin/', admin.site.urls),
+    path('blog/',include('blogs.urls')),
     
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
